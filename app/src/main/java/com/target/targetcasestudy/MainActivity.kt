fba87c933706bdc8b9b29a60cc4a629a8870715e
Sudhanshu Kumar <rishuparashar7@gmail.com>
2025-07-23 10:22:20 +0530

    status bar fix

--- app/src/main/java/com/target/targetcasestudy/MainActivity.kt
+++ app/src/main/java/com/target/targetcasestudy/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource.Companion.SideEffect
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.target.targetcasestudy.ui.navigation.NavigationStack
 import com.target.targetcasestudy.ui.navigation.Screen
 import com.target.targetcasestudy.ui.theme.ColorPrimary
+import com.target.targetcasestudy.ui.theme.ColorPrimaryDark
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,10 +25,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val statusBarColor = ColorPrimaryDark
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = statusBarColor,
+                    darkIcons = true
+                )
+            }
             Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Surface(modifier = Modifier
-                  .fillMaxSize()
-                  .padding(innerPadding)) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                ) {
                     NavigationStack(Screen.DealList)
                 }
             }
